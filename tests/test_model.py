d78980669a5a9da43518361f4f5e37ee179f204e
Martha Falkesgaard Nybroe <mfnybroe@gmail.com>
2025-01-13 10:16:48 +0100

    added system path for src code

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,11 @@
-from project1.model import MyAwesomeModel
+import os
+
+# Add the project root to the python path
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from src.project1.model import MyAwesomeModel
+
 import torch
 def test_model():
     model = MyAwesomeModel()
